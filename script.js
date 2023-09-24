e94b7503cba97023855f79f239911671c0aa4e52
rohitupskills <130683027+rohitupskills@users.noreply.github.com>
2023-09-24 23:58:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+cy.get('blockquote').eq(index).children().should('have.length', 1);
+cy.get('p').eq(index).then(ans => {
+  let text = ans.text().trim();
+  expect(text).to.eq("Who is the President of India?"); // Update the expected text here
+});
